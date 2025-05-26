8b98c3e38f121fa23a08220271660b549be06da6
rdgr1 <rodgerdias6@gmail.com>
2025-05-26 20:45:01 -0300

    feat: init repository

---
+++ api/src/test/java/org/educavagas/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.educavagas.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
